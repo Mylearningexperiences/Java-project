fa62cf8599e47f94ceb2a974c54e91f73a0ed858
Jenkins Automation Server <jenkins@sindhu72.mylabserver.com>
2018-07-31 16:53:35 +0000

    initial java commit

---
+++ src/rantangle.java
@@ -0,0 +1,18 @@
+public class Rectangle {
+  public int length;
+  public int width;
+  
+  public rectangle(int length, int width) {
+    this.length = length;
+    this.width = width;
+  }
+   public int getArea() {
+     return lengh * width;
+ }
+   public int getPerimeter() {
+     return 2 * (length + width);
+
+    }
+  }
+
+
---
+++ src/rectanulator.java
@@ -0,0 +1,15 @@
+public class Rectangulotor {
+   public static void main(String[] args) {
+      int length= Integer.parseInt(args[0]);
+      int width = Integer.parseInt(args[1]);
+        
+      rectangle myRectangle = new Ractangle(length,width);
+      
+     String output = String. format("*** your Ractangle ***\n\nLength: %d\nWidth: %d\nArea: %d\nPerimeter: %d\n\n", myRectangle.length, myRectangle.width, myRactangle.getArea(), myRectangle.getPerimeter());
+ 
+     System.out.println(output);
+
+ }
+   }
+
+  
